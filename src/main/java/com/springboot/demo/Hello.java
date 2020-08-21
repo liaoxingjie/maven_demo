d576ee77162b1c06c82f3f04061dea25138b8339
liaoxingjie <57769717@qq.com>
2020-08-21 17:11:31 +0800

    change priority from int to string

---
+++ src/main/java/com/springboot/demo/Hello.java
@@ -0,0 +1,19 @@
+package com.springboot.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(int a){
+        if (a == 1){
+            return "Hello a = 1";
+        }else if(a == 2){
+            return "Hello a = 2";
+        } else {
+            return "I don't know a = ?";
+        }
+    }
+}
---
+++ src/main/java/com/springboot/demo/entity/Area.java
@@ -0,0 +1,55 @@
+package com.springboot.demo.entity;
+
+
+import java.util.Date;
+
+public class Area {
+
+    private Integer areaId;
+    private String areaName;
+    private String priority;
+    private Date createTime;
+    private Date updateTime;
+
+
+    public Integer getAreaId() { return areaId; }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+
+
+}
